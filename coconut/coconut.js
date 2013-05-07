6c9c9beaf1bf5241ffdc93a7fab23e6a184a7d4e
Aria Bennett <twoplustwo@gmail.com>
2013-05-07 03:21:35 -0700

    Working on asetting up a server wide clock that clients sync to.

--- coconut/coconut.js
+++ coconut/coconut.js
@@ -1,19 +1,81 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to coconut.";
-  };
+//
+// Collections
+Clients = new Meteor.Collection("clients");
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+if (Meteor.isClient) {
+  // Client Globals
+  client = 0;
+  // Templates
+    Template.timeToday.get = function () {
+      Deps.autorun( function () {
+        if (Clients.findOne("default")) {
+          date = new Date();
+          client = Clients.findOne("default");
+          dateString = (
+            (date.getHours()).toString()
+            + " " + (date.getMinutes()).toString()
+            + " " + (date.getSeconds()).toString()
+          );
+          return "Beeees";
+        }
+      });
+    }
+Deps.autorun(function () {
+  Template.timeToday.helpers({
+    getTime: function () {
+      return client.time;
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+  Meteor.startup(function () {
+    // Execution sequence
+    initClientVariables();
+    startTimeUpdates();
+    // End execution sequence
+
+    function initClientVariables() {
+      //serverTime = Meteor.call("getTimeCurrent");
+    }
+    function startTimeUpdates() {
+      timeUpdateLoop = Meteor.setInterval(function () {
+        client = Clients.findOne({account: "default"});
+
+
+
+
+
+      }, 1000);
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (!Clients.findOne({account: "default"})) {
+      var currentTime = Date.now();
+      Clients.insert({
+        account: "default",
+        time: currentTime 
+      });
+    }
+    defaultUser = Clients.findOne({account: "default"});
+    timeUpdateLoop = Meteor.setInterval(function () {
+    // Time updating loop.
+      var currentTime = Date.now();
+      if (Clients.findOne({account: "default"})) {
+        Clients.update({_id: defaultUser._id}, {$set: {time: currentTime}});
+      }
+    }, 1000);
+
+
   });
 }
